a3384cf12a34cf799220521ffc47fcf907764dd3
Michael Paluch <michael.paluch@nyu.edu>
2024-04-06 18:29:27 -0400

    config defaults

--- config.php
+++ config.php
@@ -7,4 +7,12 @@ $db_password = "easypass";
 $db_name = "nyu_project";
 $db_host = "localhost";
 
+$dev_mode = TRUE;
+
+if($dev_mode) {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+}
+
 ?>
